c29f6178d73ec57941135c841f22913c90d96213
Daniel <dte.biotech46@gmail.com>
2026-03-21 00:57:40 -0400

    Finished!

--- README.md
+++ README.md
@@ -3,6 +3,8 @@ A word frequency program made in Rust
 
 # Credits
 1. [w3Schools Python](w3schools.com/python)
+2. [Wikipedia most common English words](https://en.wikipedia.org/wiki/Most_common_words_in_English)
+3. [GeeksforGeeks sort Python dictionar descending](https://www.geeksforgeeks.org/python/sort-dictionary-by-value-python-descending/)
 
 # How to Install
 
@@ -24,18 +26,21 @@ Concepts used
 - Conditional logic
 - Increment operator
 
-1. Ask the user for paragraph input
+1. Get input from text file
 2. Split the text by spaces to make an array of words
 3. Declare a frequency dictionary
 4. For each item in the array of words
-    1. If the word is a common word (a, an, the, it, etc.)
-        1. Do nothing
-    2. Else if the item is a key in the dictionary
+    1. If the item is a key in the dictionary
         1. Increment it's frequency by 1
-    3. Else
+    2. Else
         1. Declare the word key with a value of 1 
-    4. For each key in the dictionary
-        1. Print each word key along with it's frequency
+5. For each common word in the common words list
+    1. If the common word is found in the frequency dictionary
+        1. Remove it from the dictionary
+6. Sort the frequency dictionary
+7. Get the words and frequencies as arrays
+8. Loop 10 times
+    1. Print out the word and frequency at the index
 
 # Instructions
 Before pasting your text, run it through a punctuation remover. I tried removing punctuation and many other junk characters via Python's replace() and strip() methods, but both failed to actually remove the characters.
--- main.py
+++ main.py
@@ -1,12 +1,59 @@
-text_file = open("input.txt")
+# import section
+from collections import Counter
+
+# Initialization section
+
+frequency_dictionary = {}
 
 paragraph = ""
 
+common_words = [
+    "the", "be", "to", "of", "an", "a", "in", "that", "have", "I", "it",
+    "for", "not", "on", "with", "he", "as", "you", "do", "at", "this", "but", "his",
+    "by", "from", "they", "we", "say", "her", "she", "or", "an", "will", "my", "one",
+    "all", "would", "their", "there", "what", "so", "up", "out", "if", "about",
+    "who", "get", "which", "go", "me", "when", "make", "can", "like", "no", "just", "him",
+    "take", "into", "year", "your", "good", "some", "could", "them", "see", "other",
+    "than", "then", "now", "look", "only", "come", "its", "over", "think", "also",
+    "back", "after", "use", "two", "how", "our", "first", "well", "way", "even", "new",
+    "want", "because", "any", "these", "give", "day", "most", "us", "long", "tool", "before",
+    "and", "was", "company", "focus", "is", "are", "had", "more", "saying", "years",
+    "days", "people", "has", "those", "thing", "told", "wrote", "really", "things"
+]
+
+# Processing section
+
+text_file = open("input.txt")
+
 for line in text_file:
-    paragraph += line
+    paragraph += line.lower()
 
 text_file.close()
 
 paragraph_array = paragraph.split()
 
-print(paragraph_array)
+for word in paragraph_array:
+    # Easier way of either adding a key/modifying a key's value conditionally
+    frequency = frequency_dictionary.setdefault(word, 1)
+
+    new_frequency = frequency + 1
+    frequency_dictionary[word] = new_frequency
+
+for comword in common_words:
+    status = frequency_dictionary.get(comword, -999)
+
+    if status > 0: 
+        # common word has been found in dictionary, 
+        # must remove it to focus on the key words
+        frequency_dictionary.pop(comword)
+
+# sort the frequencies
+sorted_frequencies = dict(Counter(frequency_dictionary).most_common())
+
+words = list(sorted_frequencies.keys())
+frequencies = list(sorted_frequencies.values())
+
+# print the 10 most frequent words from your paragraph
+
+for index in range(10):
+    print(f"{words[index]} appeared {frequencies[index]} times")
